7784e6562e0fe517e0a3fd63834f10dc9b69b2cf
JonPalmtreeCoding <jonpalmtreegaming@gmail.com>
2014-03-24 15:21:08 -0400

    Fixes and Improvements

--- Trigonometry Basic/Trigonometry_Basic.cpp
+++ Trigonometry Basic/Trigonometry_Basic.cpp
@@ -10,14 +10,19 @@ int main(){
 	int angle = 0;
 	string function = "Default";
 
+	//Display warning that program is case-sensetive
+	cout << "Warning, this program's function entry code is case-sensetive, please type \nfunctions exactly as directed and angles as whole numbers.\n\n\n\n";
+	
 	//Ask for angle
-	cout << "Welcome to Trigonometry Basic!  Please enter an angle:\n\n";
+	cout << "Welcome to Trigonometry Basic!  Please enter an angle value:\n\n";
 	
 	//Get angle from user
 	cin >> angle;
 	
 	//Ask for function
-	cout << "\nPlease enter a function(sin/cos/tan):\n\n";
+	cout << "\nYou are using the number ";
+	cout << angle;
+	cout << ", please enter a function (Sin/Cos/Tan):\n\n";
 	
 	//Get function from user
 	cin >> function;
@@ -26,16 +31,18 @@ int main(){
 	system("cls");
 
 	//If functions to determine what function to use and output commands.
-	if (function == "sin"){
+	if (function == "Sin"){
 		SinFunc(angle);
 	}
-	if (function == "cos"){
+	if (function == "Cos"){
 		CosFunc(angle);
 	}
-	if (function == "tan"){
+	if (function == "Tan"){
 		TanFunc(angle);
 	}
-
+	else{
+		cerr << "Error! You did not specify a proper angle / function.  Please refer to the \nwarning message on program startup and try again.\n\n";
+	}
 	system("pause");
 	return 0;
 }
